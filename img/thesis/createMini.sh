d63bc0f498de4153a3caf8dfddaea4aab47037cb
MatPont <matthieu.pont@hotmail.fr>
2023-12-13 17:04:19 +0100

    add phd and update about

---
+++ img/thesis/createMini.sh
@@ -0,0 +1,1 @@
+convert \( teaser_bary2.png -resize "700x" \) \( diamondrule.png -resize "700x" \) \( teaser_pga2.png -resize "700x" \) \( diamondrule.png -resize "700x" \) \( teaser_wae2.png -resize "700x" \) -append thesis.png
